5996b63d04285f41c8e1f3dc66ea77a1da12ac17
Hassan Shahid <123826870+hassan-55HK@users.noreply.github.com>
2023-09-29 21:18:07 +0000

    conversion in datatypes

--- test/datatypes.js
+++ test/datatypes.js
@@ -10,7 +10,7 @@ let a = true;
 
 let b = false; //This is called boolean.
 
-let userEmail; // This is an undefined variable.
+let userEmail = "abcd123@gmail.com"; // This is an undefined variable.
 
 let userPassword = null; //This is null where the value is null it means that there might be some technical fault in server to fetch data.
 
@@ -42,3 +42,37 @@ console.log(arr [1] + " It will print the second value of an array because 0 is 
 //if I want to access a single value of an Object so How it is possible to do?
 
 console.log(obj.userCar);
+
+//convertion of datatypes in JS.
+
+let newEmail = Number(userEmail);
+
+console.log(typeof(newEmail))
+
+console.log(newEmail) //It would convert your string to number but it won't print its original value to console.log. It will print as NaN "Not a Number".
+
+// If you convert the value Null to number so what would it print it will print as 0.
+1
+let newPassword = Number(userPassword)
+
+console.log(typeof(newPassword))
+
+console.log(newPassword)
+
+// If you convert a boolean to number so what will it print to console if you the value of boolean is "True" it will print 1 and if the Value of Boolean is "False" it will print 0.
+
+let newA = Number(a);
+
+console.log(typeof(newA))
+
+console.log(newA);
+
+//Always remember if you convert only "32" to number so it can easily print number to console while the value is "32abc" it won't print it will print as NaN.
+
+//Same as we can also convert a number to string.
+
+let userNage = String(userAge);
+
+console.log(typeof(userNage))
+
+console.log(userNage)
