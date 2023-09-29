696e95b3a2772092c3b058b9959100c1d13396ec
Hassan Shahid <123826870+hassan-55HK@users.noreply.github.com>
2023-09-29 19:20:23 +0000

    variables completed

---
+++ test/variables.js
@@ -0,0 +1,12 @@
+const userName = "Abcd"
+let userBatch = 1;
+var userAge = 22;
+userCity = "xyz";
+
+/* we use constant in js because once we declare a value in const we can't re-declare in the same const. In simple words we use const to declare specified values. */
+
+console.log(userName);
+
+// we use console.table([]) to print more than one value together.
+
+console.table([userBatch,userAge,userCity]);
