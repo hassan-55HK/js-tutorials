39e90cfd5f45056494c097fca652782866447864
Hassan Shahid <123826870+hassan-55HK@users.noreply.github.com>
2023-09-30 22:08:31 +0000

    conversion operation completed

---
+++ test/conversionoperation.js
@@ -0,0 +1,17 @@
+//We will talk about operations in JS.
+let value = 59;
+let valueOne = -value; //Here we changed the value from positive to negative and reassigned to valueOne.
+console.log(valueOne); //It will print as -59.
+                //Following Operations in JS Below
+
+console.log(2+2);
+console.log(9-5);
+console.log(3/8);
+console.log(7%5);
+
+console.log("3" + 2 + 32 )//Guess what it will print to the console do you think it will print as 37? 
+//Not really it won't print 37 because the operation won't succeed when there comes a string at first. So it considers the whole operation as a string.
+
+console.log(2 + 5 + "23")
+// Now here what do you think it will print 30 or not as above? Yes it will because the operation will succeed when there comes a string at last.
+
