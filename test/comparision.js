c1c87d54d3211c1ccb83ce1e0ec059e3d99fe2d2
Hassan Shahid <123826870+hassan-55HK@users.noreply.github.com>
2023-10-01 16:51:51 +0000

    comparision operator completed

---
+++ test/comparision.js
@@ -0,0 +1,24 @@
+console.log(9 == 9) //it means 9 is equal to 9 ture.
+console.log(9 > 8) //it means 9 is greater than 8 ture.
+console.log(9 != 6); // it means 9 is not equal to 6 true.
+console.log(9 < 1) // it means 9 is less than 1 false.
+
+//Always remeber when you convert a number to a string and compare it to the same number as equalent to each other in simple words are their data types are same or not. it will give you the same output as below.
+
+console.log("9" == 9) //it means we have a number in string which is equal to next element 9 true.
+
+console.log(null == 0); //it means is 0 equal to null it wont print true it will print false.
+
+console.log(null > 0) //it means null is greater than 0 it wont print true it will print false because 0 isnt not less than null.
+
+console.log(null >= 0) //it means null is greater than equal to 0 it will print true.
+
+console.log(undefined == 0)// it means undefined equals to zero or not lets print to the console.log
+
+console.log(undefined != 0)// it means undefined isnt equal to zero or not lets print to the console.log
+
+console.log(undefined >= 0)// it means undefined is greater than equal to zero or not lets print to the console.log
+
+console.log(undefined <= 0)// it means undefined is less than equal  to zero or not lets print to the console.log
+
+console.log(undefined > 0)// it means undefined is greater than zero or not lets print to the console.log
