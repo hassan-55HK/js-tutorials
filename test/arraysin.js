30590511a8c2c633bc08eaf4a1903200c9a64f72
Hassan Shahid <123826870+hassan-55HK@users.noreply.github.com>
2023-10-14 07:52:44 +0000

    arrays in js updated

---
+++ test/arraysin.js
@@ -0,0 +1,16 @@
+//Here we will talk about arrays in detail.
+//We previously discussed arrays but here we will go in depth of it.
+let myArr5 = [1,2,4,4,5]
+console.log(myArr5[1])
+let myArr6 = [1,3,4,5,6]
+//How to add arrays together and print it to an array.
+//Using concat() to add two arrays together.
+myArr7 = myArr5.concat(myArr6)
+console.log(myArr7)
+//Flat(infinity) it is used to print value of array neatly.
+//for instance we declare multiple arrays in an array and we want to print those all in an array but neatly.
+// const myarr = [1,2,3["abc"],22,2,2,2[22]]
+// console.log(myarr.flat(Infinity))
+//Join() it is used to convert an array to a string.
+myArr = myArr7.join();
+console.log(myArr)
